1a32854dbb9c7a770cab43092dc29eca04e0009f
Nicolas Colque <nicolascon93@gmail.com>
2022-04-18 02:15:17 -0400

    Creación del formulario registrar usuario validado asincronamente

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginPageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterFormComponent,
   },
 ];
 
